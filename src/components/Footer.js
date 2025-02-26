6266695e1385f1fe21bade0eaeeecca12621fb0f
mimnetto <michnetto249@gmail.com>
2025-02-26 17:52:34 -0500

    First Commit
    First for Lily Portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaRegCopyright } from "react-icons/fa6";
+
+const Footer = () => (
+  <div className="footer">
+    <p className="foot"><FaRegCopyright /> Lily daMota 2025</p>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/Top.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Top = () => (
+  <><div className="footer">
+        <p></p>
+    </div><div className="top-logo"><p>.</p></div></>
+);
+
+export default Top;
