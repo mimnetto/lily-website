634202fd0fb10e009adfe7f388eccf211f098d62
mimnetto <michnetto249@gmail.com>
2025-03-12 20:07:40 -0400

    Mobile finished

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,16 +5,17 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FaRegFilePdf } from "react-icons/fa6";
 import { CiLinkedin, CiMail } from "react-icons/ci";
+import Pdf from './Portfolio25.pdf';
 
 function LilyLinks() {
   return (
     <Container>
       <Row className="justify-content-md-center">
-        <Col xs lg="2"><p className="Skill-Name"><CiLinkedin className="big-icon" /><br/><a className="Link-Text" href="https://www.linkedin.com/in/lily-damota-8488aa3b/" target="_blank" rel="noreferrer">Linkedin</a></p>
+        <Col xs lg="2"><p className="Skill-Name"><a className="Link-Text" href="https://www.linkedin.com/in/lily-damota-8488aa3b/" target="_blank" rel="noreferrer"><CiLinkedin className="big-icon" /><br/>Linkedin</a></p>
         </Col>
-        <Col xs lg="2"><p className="Skill-Name"><FaRegFilePdf className="big-icon" /><br/><a className="Link-Text" href="#" target="_blank" rel="noreferrer">Portfolio</a></p>
+        <Col xs lg="2"><p className="Skill-Name"><a className="Link-Text" href={Pdf} target="_blank" rel="noreferrer"><FaRegFilePdf className="big-icon" /><br/>Portfolio</a></p>
         </Col>
-        <Col xs lg="2"><p className="Skill-Name"><CiMail className="big-icon" /><br/>E-Mail</p>
+        <Col xs lg="2"><p className="Skill-Name"><a className="Link-Text" href="mailto:someone@example.com"><CiMail className="big-icon" /><br/>Email</a></p>
         </Col>
       </Row>
     </Container>
